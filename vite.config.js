42a78fce3c62a24353e87839de88a5dbf10ed588
Fariba-shamseh <fariba.shamseh0@gmail.com>
2025-11-09 18:42:19 +0330

    chore(vite): update Vite config settings

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'supabase': ['@supabase/supabase-js'],
+          'react-vendor': ['react', 'react-dom'],
+          'react-router': ['react-router-dom'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 });
